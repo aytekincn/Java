ff8997d5767168d30940b341322af397d8bbd708
Aytekin Can <aytekincan92@gmail.com>
2023-01-28 10:44:19 +0300

    loop

---
+++ src/switch_case/LoopDemo.java
@@ -0,0 +1,12 @@
+package switch_case;
+
+public class LoopDemo {
+    public static void main(String[] args) {
+        // for loop i=1+i ile i++ aynıdır  i +=2 ise i yi iki arttır anlamına gelir aynı şekilde azaltmak için -
+        for(int i=1; i<=10; i++){
+            System.out.println(i);
+            // döngü değeri tanımlanır ardından şartlanır en son sayacın ne kadar artacağını verdiğimiz yer
+        }
+
+    }
+}
