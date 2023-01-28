26344c21f2f63af3f3204777d5a37a23fb631663
Aytekin Can <aytekincan92@gmail.com>
2023-01-28 12:00:41 +0300

    while loop2

---
+++ src/java_lessons/WhileLoop2.java
@@ -0,0 +1,17 @@
+package java_lessons;
+
+public class WhileLoop2 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            System.out.println("Hello World:" + i++);
+        }
+        int z=1;
+        while (z<55){
+            System.out.println("Hello Universe: "+ z);
+            z++;
+        }
+    }
+
+}
+
