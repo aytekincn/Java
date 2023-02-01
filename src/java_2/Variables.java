70cef8b0b15e7325899de244c45d5039cc29bb08
Aytekin Can <aytekincan92@gmail.com>
2023-02-01 23:26:03 +0300

    Variables1

---
+++ src/java_2/Variables.java
@@ -0,0 +1,16 @@
+package java_2;
+
+public class Variables {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = a * 5;
+        int c = b - 10;
+
+        int toplam = 2 * a + b - c;
+        // verilerimiz depolandığı birimler değişkenlerdir (variables)
+
+        System.out.println ("Toplam Değer = " + toplam);
+
+
+    }
+}
