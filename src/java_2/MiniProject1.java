76dc35b33b627034afd28d69d87304bb2c491f4c
Aytekin Can <aytekincan92@gmail.com>
2023-02-04 19:28:40 +0300

    Mini_Proje1

---
+++ src/java_2/MiniProject1.java
@@ -0,0 +1,22 @@
+package java_2;
+
+import java.util.Scanner;
+
+public class MiniProject1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner (System.in);
+
+        System.out.println(" Lütfen Boyunuzu Giriniz : ( Örnek 1,80 ) ");
+
+        Double boy = s.nextDouble();
+
+        System.out.println(" Lütfen Kilonuzu Giriniz : ");
+
+        int kilo = s.nextInt();
+
+        Double bki = kilo / ( boy * boy);
+
+        System.out.println(" Beden Kitle Endeksi :" + bki);
+
+    }
+}
