e4d35e4788dc13b028f0d2da94584da5a6453113
Aytekin Can <aytekincan92@gmail.com>
2023-02-04 17:48:51 +0300

    Girdi alma

---
+++ src/java_2/New_S4.java
@@ -0,0 +1,16 @@
+package java_2;
+
+import java.util.Scanner;
+
+public class New_S4 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner (System.in);
+
+        int yas1 = s.nextInt();
+        int yas2 = s.nextInt();
+        int yas3 = s.nextInt();
+
+        System.out.println(" Yaş1: " + yas1 + " Yaş2: " + yas2 + " Yaş3: " + yas3);
+        // Girdiyi hem enter kullanarak hem de boşluk kullanarak girebiliriz.
+    }
+}
