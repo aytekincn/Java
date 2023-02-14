0c94664855fedbed329813ebb78144079f5fcfea
Aytekin Can <aytekincan92@gmail.com>
2023-02-14 17:38:45 +0300

    Conditions2

---
+++ src/java_2/Conditions2.java
@@ -0,0 +1,32 @@
+package java_2;
+
+import java.util.Scanner;
+
+public class Conditions2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner (System.in);
+        System.out.print("Lütfen Notunuzu Girin :");
+        int not = s.nextInt();
+
+        if ( not >= 90 ) {
+            System.out.println("Notunuz AA'dır");
+
+        }else if ( not >= 80 ) {
+            System.out.println("Notunuz BB'dir ");
+
+        }else if ( not >= 70 ) {
+            System.out.println("Notunuz CC'dir");
+
+        }else if ( not >= 60) {
+            System.out.println("Notunuz DC'dir");
+
+        }else {
+            System.out.println("Kaldınız, Notunuz FF'dir");
+        }
+
+
+
+
+
+    }
+}
