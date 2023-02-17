8a36f8b6a598c35ec1a9023e20a87fe36c838b3c
Aytekin Can <aytekincan92@gmail.com>
2023-02-17 19:26:34 +0300

    Break

---
+++ src/java_2/Break_Continue.java
@@ -0,0 +1,26 @@
+package java_2;
+
+import java.util.Scanner;
+
+public class Break_Continue {
+    public static void main (String[] args) {
+        Scanner s = new Scanner (System.in);
+        while ( true ) {
+            System.out.print("Bir Sayı Girin :");
+            int islem = s.nextInt();
+            if ( islem == -1 ) {
+                System.out.println("İşlem sonlandı..." + islem );
+                break;
+
+            }
+            System.out.print("İkinci Sayıyı Girin :");
+            int islem2 = s.nextInt();
+            int toplam = islem + islem2;
+
+            System.out.println("Toplamı : " + toplam  );
+
+        }
+
+
+    }
+}
