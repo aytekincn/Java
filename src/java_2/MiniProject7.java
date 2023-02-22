dbadb4d7c066e700b62abc3487bcea8462087656
Aytekin Can <aytekincan92@gmail.com>
2023-02-22 21:47:26 +0300

    ATM

---
+++ src/java_2/MiniProject7.java
@@ -0,0 +1,18 @@
+package java_2;
+
+public class MiniProject7 {
+    public static void main(String[] args) {
+
+        for ( int  a = 1; a <= 10; a++) {
+
+            for ( int  b = 1; b <= 10; b++) {
+                System.out.println( a + " x " + b );
+
+
+            }
+
+
+        }
+
+    }
+}
