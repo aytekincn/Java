bc7cf3eab65f716640a249c8663cbe3358045976
Aytekin Can <aytekincan92@gmail.com>
2023-02-23 09:18:43 +0300

    Project8

--- src/java_2/MiniProject8.java
+++ src/java_2/MiniProject8.java
@@ -5,14 +5,19 @@ import java.util.Scanner;
 public class MiniProject8 {
     public static void main(String[] args) {
         Scanner s = new Scanner (System.in);
+
         System.out.println("Bankamıza Hoşgeldiniz... Faiz oranı %6 ");
         int anapara, vade;
+
         System.out.print("Yatırmak İstediğiniz Tutar :");
         anapara = s.nextInt();
+
         System.out.print("Kaç Yıl Vadeli Yatırmak İstiyorsunuz ? ");
         vade = s.nextInt();
+
         double toplampara = anapara;
         double faizOrani = 0.06;
+
         for ( int a = 1 ; a <= vade ; a++ ) {
             toplampara = ( toplampara * faizOrani ) + toplampara;
             System.out.println( a + ". yılın sonunda toplam para :" + ( int ) toplampara );
