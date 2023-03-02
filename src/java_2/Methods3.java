a12cccc361e6e88e02ee6557188a4516131ef236
Aytekin Can <aytekincan92@gmail.com>
2023-03-02 13:41:41 +0300

    Return Method

---
+++ src/java_2/Methods3.java
@@ -0,0 +1,19 @@
+package java_2;
+
+public class Methods3 {
+    public static int ikiilecarp ( int a ) {
+        return a * 2;
+    }
+    public static int ikiiletopla ( int a ) {
+        return a + 2;
+    }
+    public static int dortilecarp ( int a ) {
+        return a * 4;
+    }
+    public static void main(String[] args) {
+
+        System.out.println ( dortilecarp ( ikiiletopla ( ikiilecarp(6) ) ) );
+        // Fonksiyonların içinde method kullanılacaksa void iç içe çalıştırmalarda çalışmaz bundan dolayı method kullanırız.
+
+    }
+}
