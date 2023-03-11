b349bb51f9f643ae95f125fd8abf0cb626507436
Aytekin Can <aytekincan92@gmail.com>
2023-03-11 20:13:39 +0300

    OOP

---
+++ src/OOP/Car.java
@@ -0,0 +1,56 @@
+package OOP;
+
+public class Car {
+    // Encapsulation (Sarmalama)
+    private String renk;
+    private int kapilar;
+    private int tekerlek;
+    private String motor;
+    private String model;
+
+    public void setModel ( String model ) {
+        this.model = model;  // Private alanlara ulaşmak için kullanılan yöntem. Eğer method ismi aynı değilse this. kullanmaya gerek yok
+        // Set kısmı değer döndürmez  setter metodu sadece değiştirme işlemi yapacağı için void olarak tanımlanır.
+    }
+    public String getModel () {
+        return this.model; // Getter metotları geri dönüşü olan metot tipindedir ve isimlendirilmesi ise get ile başlayıp sonra değişken ismi yazılmalıdır.
+        // Bu değişkenlere dışarıdan erişebilmek için her bir değişkenimiz için Getter metodu yazmalıyız.
+    }
+
+    public String getRenk() {
+        return renk;
+    }
+
+    public void setRenk(String renk) {
+        this.renk = renk;
+    }
+
+    public int getKapilar() {
+        return kapilar;
+    }
+
+    public void setKapilar(int kapilar) {
+        this.kapilar = kapilar;
+    }
+
+    public int getTekerlek() {
+        return tekerlek;
+    }
+
+    public void setTekerlek(int tekerlek) {
+        this.tekerlek = tekerlek;
+    }
+
+    public String getMotor() {
+        return motor;
+    }
+
+    public void setMotor(String motor) {
+        this.motor = motor;
+    }
+    /* Refactor kısmanda Encapsulate fields kısmını kullanarak diğer özellikeri de encapsulation yapabiliyoruz.
+
+     */
+
+
+}
---
+++ src/OOP/Test.java
@@ -0,0 +1,24 @@
+package OOP;
+
+public class Test {
+    public static void main(String[] args) {
+        Car araba1 = new Car (); //Class'a ulaşmak için bu şekilde bir obje oluşturuyoruz.
+
+        araba1.setModel("Bentley");
+        System.out.println("Arabanın modeli :" + araba1.getModel() );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
